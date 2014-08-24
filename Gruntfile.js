25f7d616a89e322f75febee5555ef223ef96aed9
Giancarlo Soverini <giancarlosoverini@gmail.com>
2014-08-24 22:39:06 +0200

    Adding Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    clean: {
+      files: ['dist/<%= pkg.name %>']
+    },
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: [
+              'src/intro.js',
+              'src/outro.js'
+             ],
+        dest: 'dist/<%= pkg.name %>/jquery.<%= pkg.name %>.js'
+      },
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/<%= pkg.name %>/jquery.<%= pkg.name %>.min.js'
+      },
+    },
+    jshint: {
+       files: ['Gruntfile.js', 'src/**/*.js'],
+      // configure JSHint (documented at http://www.jshint.com/docs/)
+      options: {
+          // more options here if you want to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true
+        }
+      }
+    },
+    watch: {
+      jshint: {
+        files: ['Gruntfile.js,<%= jshint.files %>'],
+        tasks: ['jshint']
+          }
+      },
+      concat: {
+        files: ['<%= concat.dist.src %>'],
+        tasks: ['concat']
+      }
+       
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'clean', 'concat', 'uglify']);
+
+};
